89065c4f8dcf8d435a5567a0a3ddaee559891c4e
Yusa <yusa.oruc@stu.fsm.edu.tr>
2023-09-12 18:17:31 +0300

    Custom Directives Product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ProductComponent } from './product.component';
 import { FormsModule } from '@angular/forms';
 import { SummaryPipe } from './summary.pipe';
+import { InputEmailDirective } from './input-email.directive';
 
 @NgModule({
   declarations: [
     ProductComponent,
-    SummaryPipe
+    SummaryPipe,
+    InputEmailDirective
   ],
   imports: [
     BrowserModule,
