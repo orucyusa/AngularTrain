b6b0de841dcd0bd7eee8b1ca042c8728363fbabf
Yusa <yusa.oruc@stu.fsm.edu.tr>
2023-09-12 14:46:19 +0300

    Class-Binding-added.

--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -9,6 +9,21 @@ import { Product } from './product.model';
 
 export class ProductComponent {
     model: ProductRepository = new ProductRepository();
+    disabled=false;
 
-    product: Product = this.model.getProductsById(1);
+    getClasses(id: number): string {
+       let product = this.model.getProductsById(id);
+       return (product.price <=1000 ? "bg-info" : "bg-secondary") + " m-2 p-2 text-white";
+    }
+
+    getClassMap(id: number): Object {
+        let product = this.model.getProductsById(id);
+
+        return {
+            "bg-info": product.price <= 1000,
+            "bg-secondary": product.price > 1000,
+            "text-center text-white": product.name == "Samsung S6" 
+        }
+    }
+
 }
