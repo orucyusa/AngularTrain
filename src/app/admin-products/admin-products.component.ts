8d79685c36ac75093c65ee18e689b4caa44f9925
Yusa <yusa.oruc@stu.fsm.edu.tr>
2023-09-12 19:32:31 +0300

    Display-Selected + Angular Template Variables added.

--- src/app/admin-products/admin-products.component.ts
+++ src/app/admin-products/admin-products.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
 import { ProductRepository } from '../repository.model';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
-  styleUrls: ['./admin-products.component.css']
+  styleUrls: ['admin-products.component.css']
 })
 export class AdminProductsComponent {
-  
   products;
   model: ProductRepository;
+  selectedProduct: any;
   constructor() {
     this.model = new ProductRepository();
     this.products = this.model.getProducts();
+  }
+
+  getSelected(product: Product): boolean {
+    return product.name == this.selectedProduct;
   }
 
 }
