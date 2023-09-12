1d3187d78d08eda23a4066d8c3e1b1734013c7c9
Yusa <yusa.oruc@stu.fsm.edu.tr>
2023-09-12 16:23:31 +0300

    Angular-NgFor-

--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -3,13 +3,11 @@ import { ProductRepository } from "./repository.model";
 import { Product } from './product.model';
 @Component({
     selector: "app",
-    templateUrl: 'product.component.html',
+    templateUrl: './product.component.html',
     styleUrls: ["product.component.css"]
 })
 
 export class ProductComponent {
    model: ProductRepository = new ProductRepository();
-
-   productName: string = this.model.getProductsById(1).name;
 
 }
