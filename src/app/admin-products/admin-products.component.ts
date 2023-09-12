1f09969c9dc0347c8d1a19fe667b424cba035333
Yusa <yusa.oruc@stu.fsm.edu.tr>
2023-09-12 20:59:14 +0300

    Showing-Product-Details And Product

--- src/app/admin-products/admin-products.component.ts
+++ src/app/admin-products/admin-products.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ProductRepository } from '../repository.model';
 import { Product } from '../product.model';
 
@@ -8,16 +8,31 @@ import { Product } from '../product.model';
   styleUrls: ['admin-products.component.css']
 })
 export class AdminProductsComponent {
+  @ViewChild('pName') pName: ElementRef;
+
   products;
   model: ProductRepository;
-  selectedProduct: any;
+  selectedProduct: Product | null = null;
   constructor() {
     this.model = new ProductRepository();
     this.products = this.model.getProducts();
   }
 
   getSelected(product: Product): boolean {
-    return product.name == this.selectedProduct;
+    return product == this.selectedProduct;
+  }
+  
+  editProduct(product: Product) {
+      this.selectedProduct = product;
+  }
+  updateSelectedProduct(propertyName: string, newValue: string): void {
+    this.selectedProduct[propertyName] = newValue;
   }
 
+  saveChanges(name, description, price, imageUrl){
+    this.updateSelectedProduct('description', description);            
+    this.updateSelectedProduct('name', name);
+    this.updateSelectedProduct('price', price);
+    this.updateSelectedProduct('imageUrl', imageUrl);
+  }
 }
