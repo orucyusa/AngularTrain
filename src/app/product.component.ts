b239a6d83006ae63648cf8883bffd08a85f79428
Yusa <yusa.oruc@stu.fsm.edu.tr>
2023-09-12 15:01:34 +0300

    Event-Binding added.

--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -9,31 +9,14 @@ import { Product } from './product.model';
 
 export class ProductComponent {
     model: ProductRepository = new ProductRepository();
-    disabled=false;
 
-    getClasses(id: number): string {
-       let product = this.model.getProductsById(id);
-       return (product.price <=1000 ? "bg-info" : "bg-secondary") + " m-2 p-2 text-white";
+    onSubmit($event) {
+        $event.stopPropagation();
+        console.log('button was clicked');
+        console.log($event);
     }
-
-    getClassMap(id: number): Object {
-        let product = this.model.getProductsById(id);
-
-        return {
-            "bg-info": product.price <= 1000,
-            "bg-secondary": product.price > 1000,
-            "text-center text-white": product.name == "Samsung S6" 
-        }
-    }
-
-    color: string = this.model.getProductsById(1).price <= 2000 ? "green" : "red";
-    fontSize: string = "25px";
-
-    getStyles(id: number) {
-        let product = this.model.getProductsById(id);
-        return {
-            fontSize: "25px",
-            color: product.price <=1000 ? "green" : "red"
-        }
+    
+    onDivClicked() {
+        console.log('div was clicked');
     }
 }
